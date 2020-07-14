935d71538bd3ce2605ff886dd12c0d00aee061b9
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-07-14 23:17:34 +0100

    domain config

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "seemy-energy"
+
+    workspaces {
+      name = "dns"
+    }
+  }
+}
+
+provider "cloudflare" {
+  version = "~> 2.0"
+  email   = "chris@cns.me.uk"
+}
+
+resource "cloudflare_zone" "zone" {
+  zone = "seemy.energy"
+  plan = "free"
+}
+
+resource "cloudflare_record" "A" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "www"
+  value   = "45.55.72.95"
+  type    = "A"
+  ttl     = 3600
+}
+
+
+resource "cloudflare_record" "A" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "@"
+  value   = "45.55.72.95"
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "txt" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "_redirect"
+  value   = "Redirects from /* to https://github.com/seemy-energy/*"
+  type    = "TXT"
+  ttl     = 3600
+}
